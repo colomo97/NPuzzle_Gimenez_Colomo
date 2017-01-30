ef6ce281583a2afcb657c48ea56747f5d6e37e08
colomo97 <davidcolomo.dam@gmail.com>
2017-01-30 19:16:50 +0100

    //Menu Created

--- app/src/main/java/dam2/fje/edu/npuzzle_gimenez_colomo/PrincipalActivity.java
+++ app/src/main/java/dam2/fje/edu/npuzzle_gimenez_colomo/PrincipalActivity.java
@@ -1,7 +1,12 @@
 package dam2.fje.edu.npuzzle_gimenez_colomo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class PrincipalActivity extends AppCompatActivity {
 
@@ -11,4 +16,34 @@ public class PrincipalActivity extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
         //hi
     }
+
+    /**
+     * Redifinició del mètode per a crear el memú d'opcions.
+     * @param menu
+     * @return menu
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * Mètode per a saber l'item seleccionat del menú
+     * @param item
+     * @return Retorna la selecció escollida
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_settings:
+                Toast.makeText(PrincipalActivity.this, "SETTINGS", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
 }

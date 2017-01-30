4196f9050f7164b9c7433287383159b4acea6d0d
colomo97 <davidcolomo.dam@gmail.com>
2017-01-30 20:40:12 +0100

    //Music

--- app/src/main/java/dam2/fje/edu/npuzzle_gimenez_colomo/PrincipalActivity.java
+++ app/src/main/java/dam2/fje/edu/npuzzle_gimenez_colomo/PrincipalActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 public class PrincipalActivity extends AppCompatActivity {
     static private boolean imageSelector = true;
     MediaPlayer musicaFons;
+    //jjajajasalu2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

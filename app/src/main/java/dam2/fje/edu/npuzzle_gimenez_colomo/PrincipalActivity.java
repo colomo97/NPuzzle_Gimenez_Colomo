d75f8c80cf6ba36f17c5c62ea7c9036b2e96cdde
colomo97 <davidcolomo.dam@gmail.com>
2017-01-30 19:18:18 +0100

    //Menu Created

--- app/src/main/java/dam2/fje/edu/npuzzle_gimenez_colomo/PrincipalActivity.java
+++ app/src/main/java/dam2/fje/edu/npuzzle_gimenez_colomo/PrincipalActivity.java
@@ -14,7 +14,7 @@ public class PrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        //hi
+        //Menu Created
     }
 
     /**
